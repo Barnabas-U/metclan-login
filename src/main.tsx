4f6646458cff12460f0226d1cd1af7f1da1c3057
Barnabas Ukagha <mcbarna18@gmail.com>
2024-08-22 08:26:18 +0100

    Update main.tsx
    added the publish-key to the main project

--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,7 @@ if (!PUBLISHABLE_KEY) {
 }
 
 createRoot(document.getElementById('root')!).render(
-  <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+  <ClerkProvider publishableKey={'pk_test_bGlnaHQtdGFycG9uLTM5LmNsZXJrLmFjY291bnRzLmRldiQ'} afterSignOutUrl="/">
      <StrictMode>
       <App />
      </StrictMode>
